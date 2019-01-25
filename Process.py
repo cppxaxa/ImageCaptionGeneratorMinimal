7d23a1f9175a394f1c0de919c5f1860cb27ddd12
Christ Prateek Prasanna Xaxa <cppxaxa@live.com>
2019-01-26 02:47:59 +0530

    second commit

--- Process.py
+++ Process.py
@@ -1,11 +1,16 @@
 from core.backend import ModelWrapper
+import cv2
 
 model_wrapper = ModelWrapper()
 
-image_data = None
-with open('input.jpg', 'rb') as f:
-    image_data = f.read()
-    
+img = cv2.imread('input.jpg')
+image_data = cv2.imencode('.jpg', img)[1].tostring()
+
+print(image_data)
+
+# with open('input.jpg', 'rb') as f:
+#     image_data = f.read()
+
 preds = model_wrapper.predict(image_data)
 
 label_preds = [{'index': p[0], 'caption': p[1], 'probability': p[2]} for p in preds]
